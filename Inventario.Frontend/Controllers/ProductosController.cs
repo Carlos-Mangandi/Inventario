351ecfe575c646ce3260ce5c7224be4004438334
Carlos Mangandí <carlosmescobar46@gmail.com>
2024-06-21 11:01:26 -0300

    Creación de nuestros models en la parte frontend y unas configuraciones para nuestras modales

---
+++ Inventario.Frontend/Controllers/ProductosController.cs
@@ -0,0 +1,31 @@
+﻿using Inventario.Frontend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Inventario.Frontend.Controllers
+{
+    public class ProductosController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public ProductosController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+            _httpClient.BaseAddress = new Uri("https://localhost:7184/");
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var response = await _httpClient.GetAsync("/api/Productos");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var productos = JsonConvert.DeserializeObject<IEnumerable<Producto>>(content);
+                return View("Index", productos);
+            }
+
+            return View(new List<Producto>());
+        }
+    }
+}
